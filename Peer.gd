83f23e198aea2cf6df7a5855b1e8a2e51e85c729
忘忧の <61624558+Daylily-Zeleen@users.noreply.github.com>
2022-02-12 22:53:54 +0800

    将默认场景改为 Ping 测试

--- Peer.gd
+++ Peer.gd
@@ -1,41 +1,65 @@
 extends RealtimeClient
 
+# 连接到服务器
 func _ready() -> void:
-	connect("connected_to_master",self,"_on_connect_to_master")
-	connect("room_joined",self,"_on_room_joined")
-	connect("event_received",self,"_on_event_received")
-	connect("player_room_entered",self,"_on_player_room_entered")
-	self.bg_dispatch_interval_ms = 20
-	self.bg_send_interval_ms = 20
-	self.bg_dispatch = true
-	self.bg_send = true
-	var setting = AppSettings.new()
+	var setting = AppSettings.new()	
 	setting.app_id_realtime = "c45c78da-788d-407b-b249-334de949c480"
 	setting.app_version = "v0.1"
 	setting.fixed_region = "cn"
+	setting.use_name_server = true
 	setting.server = "ns.photonengine.cn"
 	connect_using_settings(setting)
-
-func _on_connect_to_master():
-	print("连接到主服务器成功:",name)
 	
+# 每帧处理网络
+func _physics_process(delta: float) -> void:
+	service()
+
+# 连接到服务器时加入房间
+func _on_connected_to_master()->void:
 	var params = EnterRoomParams.new()
-	params.room_name = "test_room"
+	params.room_name = "ping_test_room"
 	params.room_options = RoomOptions.new()
-	params.room_options.max_players = 2
+	params.room_options.max_players = 2 # 限定两名玩家，只做 ping test
 	op_join_or_create_room(params)
+	
+var ping_start:int
+func _on_room_joined()->void:
+	print("进入房间")
+	# 由后加入的玩家向房主进行测试
+	if not self.local_player.is_master_client:
+		# 记录 开始时间戳
+		ping_start = OS.get_ticks_msec()
+		# 发送
+		if not op_raise_event(0,"",RaiseEventOptions.Prefeb.DEFAULT,SendOptions.Prefeb.SEND_RELIABLE):
+			printerr("发送失败")
 
-var start_tick :int =0
-func _on_room_joined():
-	var raise_event_options:=RaiseEventOptions.new()
-	raise_event_options.receivers = Photon.ReceiverGroup.All
-	start_tick = OS.get_ticks_msec()
-	op_raise_event(0, "abc".to_ascii(), raise_event_options, SendOptions.Prefeb.SEND_UNRELIABLE)
-
-func _on_event_received(event_data:EventData):
+var timeout := 1.0
+func _process(delta: float) -> void:
+	if ping_start != 0:
+		timeout -= delta
+		if timeout <=0:
+			var interval := OS.get_ticks_msec() - ping_start
+			if interval > 1000:
+				printerr("-- 超过 %s ms 未接受到回复--"%interval)
+			timeout = 1
+			
+func _on_event_received(event_data: EventData)->void:
 	if event_data.code == 0:
-		print(OS.get_ticks_msec() - start_tick)
-	print("接收事件:",name," ",event_data.custom_data )
+		# 回复
+		if not op_raise_event(1,"",RaiseEventOptions.Prefeb.DEFAULT, SendOptions.Prefeb.SEND_RELIABLE):
+			printerr("回复失败")
+	elif event_data.code == 1:
+		# 接收回复
+		print("与房主往返延迟: %d ms"%(OS.get_ticks_msec() - ping_start))
+		ping_start = 0
+		# 2秒后再次发送
+		yield(get_tree().create_timer(2.0),"timeout")
+		# 记录 开始时间戳
+		ping_start = OS.get_ticks_msec()
+		# 发送
+		if not op_raise_event(0,"",RaiseEventOptions.Prefeb.DEFAULT,SendOptions.Prefeb.SEND_RELIABLE):
+			printerr("发送失败")
 
-func _on_player_room_entered(new_player:PhotonPlayer):
-	print(new_player)
+
+	
+
